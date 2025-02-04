4daeb558efdf270597dc65373d5063cb2a226882
Vess Nedkov <2430850+vnedkov@users.noreply.github.com>
2025-02-04 14:51:37 -0500

    Removed unnecessry providers

--- providers.tf
+++ providers.tf
@@ -6,10 +6,6 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "2.35.1"
     }
-    kustomization = {
-      source  = "kbst/kustomization"
-      version = "0.9.0"
-    }
     # Why do I need another kubernetes provider? 
     # Because kubectl does not need cluster configuration in the planning phase.
     # However it does not have all other kubernetes provider features.
@@ -24,18 +20,6 @@ terraform {
     talos = {
       source  = "siderolabs/talos"
       version = "0.7.0"
-    }
-    restapi = {
-      source  = "Mastercard/restapi"
-      version = "1.20.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "2.13.1"
-    }
-    checkmate = {
-      source  = "tetratelabs/checkmate"
-      version = "1.8.4"
     }
   }
 }
@@ -78,24 +62,6 @@ provider "kubectl" {
   load_config_file       = false
 }
 
-provider "kustomization" {
-  kubeconfig_raw = module.talos.kube_config.kubeconfig_raw
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = module.talos.kube_config.kubernetes_client_configuration.host
-    cluster_ca_certificate = local.cluster_ca_certificate
-    client_certificate     = local.client_certificate
-    client_key             = local.client_key
-  }
-}
-
 provider "http" {
   
-}
-
-# Simple provider for checking the cluster availability over http
-provider "checkmate" {
-  # no configuration required
 }
